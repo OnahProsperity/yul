972a35344b2fdf4b1ae048ce4bf0e6cfc20e5622
onahprosperity <prosperauthor@gmail.com>
2022-12-17 10:10:52 +0100

    read and write to fixed length array

--- contracts/yul_storageWithMappingAndArray.sol
+++ contracts/yul_storageWithMappingAndArray.sol
@@ -10,10 +10,27 @@ contract StoragePart3 {
     // Mapping ==> Array
     // Mapping ==> Struct
 
+    uint256[3] fixedArray;
+
 
     constructor() {
+        fixedArray = [10,80,90];
 
     }
-    
+
+    function readFixedArray(uint256 index) external view returns(uint256 ret) {
+        assembly {
+            let slot := fixedArray.slot
+            ret := sload(add(slot, index))
+        }
+    }
+
+    function writeToFixedArray(uint256 index, uint256 value) external {
+        assembly {
+            let slot := fixedArray.slot
+            sstore(add(slot, index), value)
+        }
+    }
+
     
 }
